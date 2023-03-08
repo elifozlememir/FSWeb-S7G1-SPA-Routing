41d39acb39ed6796de20f6b44329dfbef8e07937
elifozlememir <elifozlememir@gmail.com>
2023-03-08 13:00:01 +0100

    “S7G1“

--- client/src/Filmler/KaydedilenlerListesi.js
+++ client/src/Filmler/KaydedilenlerListesi.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function KaydedilenlerListesi(props) {
+
+  const navigate=useNavigate();
+
   return (
     <div className="saved-list">
       <h3>Kaydedilen Filmler:</h3>
       {props.list.map(movie => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">Anasayfa</div>
+      <div onClick={()=>navigate("/")} className="home-button">Anasayfa</div>
     </div>
   );
 }
